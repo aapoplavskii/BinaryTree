b2651d5b9dddd12aaca746122108bb657cad74f7
Andrey Poplavskii <andris838@gmail.com>
2021-10-19 18:39:27 +0300

    Initial commit

---
+++ Tree/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                WorkWithData WorkData = new WorkWithData();
+                var currentNode = WorkData.EnterDataMethod();
+
+                Console.WriteLine("Итоговое дерево зарплат:");
+                WorkData.Traverse(currentNode);
+
+                WorkData.FindZP(currentNode);
+
+
+
+                while (true)
+                {
+                Console.WriteLine();
+                    Console.WriteLine("Для ввода новых данных по сотрудникам нажмите 0.");
+                    Console.WriteLine("Для нового поиска зарплаты нажмите 1.");
+                    Console.WriteLine("Для выхода из программы нажмите 2.");
+
+                    var otvet = Console.ReadKey();
+                    Console.WriteLine();
+
+                    switch (otvet.KeyChar)
+                    {
+                        case '1':
+                            WorkData.FindZP(currentNode);
+                            break;
+                        case '0':
+                            Console.Clear();
+                            currentNode = WorkData.EnterDataMethod();
+                            Console.WriteLine("Итоговое дерево зарплат:");
+                            WorkData.Traverse(currentNode);
+                            WorkData.FindZP(currentNode);
+                        break;
+                        case '2':
+                            break;
+                        default:
+                            Console.WriteLine("Необходимо ввести указанные цифры!");
+                            break;
+
+                    }
+
+                    if (otvet.KeyChar == '2')
+                    {
+                        break;
+                    }
+
+                }
+
+            
+
+           
+
+
+        }
+
+        
+    }
+}
---
+++ Tree/WorkWithData.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Tree
+{
+    public class WorkWithData
+    {
+
+        public Node EnterDataMethod()
+        {
+            string _name = string.Empty;
+            int _zp = 0;
+            bool result;
+
+            Node root = null;
+
+
+            while (true)
+            {
+                Console.Write("Введите имя сотрудника: ");
+                _name = Console.ReadLine();
+                
+                if (_name == "")
+                {
+                    break;
+                }
+
+                Console.WriteLine();
+
+                do
+                {
+                    Console.Write("Введите зарплату сотрудника: ");
+                    var str = Console.ReadLine();
+
+                    result = int.TryParse(str, out _zp);
+
+                    if (!result)
+                    { 
+                        Console.WriteLine($"Зарплату необходимо вводить цифрами! Повторите ввод зарплаты для {_name}");
+                        Console.WriteLine();
+                    }
+
+                } while (!result);
+
+                
+                
+                
+                Console.WriteLine();
+
+                if (root == null)
+                {
+
+                    root = new Node
+                    {
+                        Name = _name,
+                        ZP = _zp
+                    };
+
+                }
+                else
+                {
+                    Add(root, new Node
+                    {
+                        Name = _name,
+                        ZP = _zp
+                    });
+
+                }
+
+            }
+        
+            return root;
+
+        }
+
+        public Node Seach(Node current, int zp)
+        {
+            if (zp < current.ZP)
+            {
+
+                if (current.LeftNode != null)
+                {
+
+                    return Seach(current.LeftNode, zp);
+
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            else if (zp > current.ZP)
+            {
+                if (current.RightNode != null)
+                {
+
+                    return Seach(current.RightNode, zp);
+
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return current;
+            }
+
+
+
+        }
+
+        public void Add(Node currentNode, Node addedNode)
+        {
+
+            if (addedNode.ZP < currentNode.ZP)
+            {
+                if (currentNode.LeftNode != null)
+                {
+                    Add(currentNode.LeftNode, addedNode);
+                }
+                else
+                {
+                    currentNode.LeftNode = addedNode;
+                }
+            }
+            else
+            {
+                if (currentNode.RightNode != null)
+                {
+                    Add(currentNode.RightNode, addedNode);
+                }
+                else
+                {
+                    currentNode.RightNode = addedNode;
+                }
+            }
+
+        }
+
+        public void Traverse(Node node)
+        {
+            
+            if (node.LeftNode != null)
+            {
+                Traverse(node.LeftNode);
+            }
+
+            Console.WriteLine(node);
+
+            if (node.RightNode != null)
+            {
+
+                Traverse(node.RightNode);
+            }
+
+
+        }
+
+        public void FindZP(Node currentNode)
+        {
+            int zp = 0;
+            bool result;
+
+            do
+            {
+            
+                Console.WriteLine();
+                Console.Write("Введите размер зарплаты для поиска в базе: ");
+                var strzp = Console.ReadLine();
+                
+            result = int.TryParse(strzp, out zp);
+
+            if (!result)
+            {
+                Console.WriteLine("Для поиска зарплаты значение необходимо вводить цифрами. Повторите ввода снова!");
+                Console.WriteLine();
+            }
+
+            } while (!result);
+
+          
+
+            var Findznach = Seach(currentNode, zp);
+            if (Findznach != null)
+            {
+
+                Console.WriteLine($"Имя сотрудника с указанной зарплатой - {Findznach.Name}");
+                Console.WriteLine();
+
+            }
+            else
+            {
+
+                Console.WriteLine("Такой сотрудник не найден");
+                Console.WriteLine();
+
+            }
+
+
+
+        }
+
+    }
+}
